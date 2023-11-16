048a4a3eb2d41a1d93916a77cebe60f71034ecd1
AniTigranyan <144479495+AniTigranyan@users.noreply.github.com>
2023-11-16 22:42:54 +0400

    Add files via upload

--- homework.js
+++ homework.js
@@ -1,87 +1,77 @@
-//1.Given a number. Print “odd” if the number is odd and “even” if it’s even.
-num = 123
-if (num % 2 === 0) {console.log("even")}
-else; {
-    console.log("odd")
+//1.Given a number print its digits count. Use both loops (for, while)
+function countDigits(number) {
+    let digitCount = 0
+    const numberStr = number.toString()
+
+    for (let i = 0; i < numberStr.length; i++) {
+        digitCount++
+    }
+    console.log(digitCount)
 }
 
-num1 = 35 
-if (num1 % 2 === 0) {console.log("even")}
-else; {
-    console.log("odd")
+
+
+//2.Count numbers of digit 9 in a number. Use both loops (for, while)
+function digit9(number) {
+    let numStr = number.toString()
+    let count = 0
+    for (let i = 0; i < numStr.length; i++)
+    if (numStr[i] === '9') {
+        count++
+    }
+    console.log(count)
 }
 
-num2 = 70;
-if (num2 % 2 === 0) {console.log("even")}
-else; {
-    console.log("odd")
+function countdigit9(number) {
+    let numStr = number.toString()
+    let count = 0 
+    let i = 0
+    while(i < numStr.length) {
+    if (numStr[i] === '9'){
+    count++
+    }
+    i++
+  }
+    console.log(count)
 }
 
-//2.Given two numbers print 1 if one of them is divisible by the other one, otherwise print 0.
-let a = 3
-let b = 14
-if (a % b === 0 || b % a === 0) {
-    console.log(1)
-}
-else; {
-    console.log(0)
-}
 
-let a1 = 18
-let b1 = 2
-if (a1 % b1 === 0 || b1 % a1 === 0) {
-    console.log(1)
-}
-else; {
-    console.log(0)
+//3.Given two numbers. Print ony odd numbers between that numbers.
+function printOnlyOddNumbers(start, end) 
+{
+    let number = start 
+    while(number <= end) {
+        if (number % 2 !== 0) {
+          console.log(number)
+        }
+        number++
+    }   
 }
 
-let a2 = 7
-let b2 = 21
-if (a2 % b2 === 0 || b2 % a2 === 0) {
-    console.log(1)
+//4.Given a number as input, insert dashes (-) between each two even numbers.
+function InsertDashesBetweenEvens(number) {
+    const numStr = number.toString()
+    let result = numStr[0]
+
+    for(let i = 1; i < numStr.length; i++) {
+        const currentDigit = numStr[i]
+        const prevDigit = numStr[i - 1]
+        
+        if(currentDigit % 2 === 0 && prevDigit % 2 === 0) {
+            result += `-${currentDigit}`
+        } else {
+            result += currentDigit
+        }
+    }
+    console.log(result)
 }
-else; {console.log(0)
-}
 
-//3.Given a positive integer. Bring the last digit of the number to the beginning. Print the new
-//number. If the last digit of the inserted number is 0, number remains the same.
-
-
-//4.Given five numbers as input. Calculate and print the average of the numbers(without
-//using arrays)
-
-let nums1 = 45
-let nums2 = -12
-let nums3 = 0
-let nums4 = 3
-let nums5 = - 15
-
-let sum = nums1 + nums2 + nums3 + nums4 + nums5
-let count = 5
-let average = sum / count 
-console.log("The average of the numbers is: " + average)
-
-let num6 = 7
-let num7 = 52
-let num8 = -23
-let num9 = 9
-let num10 = -81
-
-let sum1 = num6 + num7 + num8 + num9 + num10
-let count1 = 5
-let average1 = sum1 / count1
-console.log("The average of the numbers is: " + average1)
-
-//5.Given the following code rewrite it using only two if operators. (Hint: use logical operators)
-
-var n = +prompt();
-var i = 0;
-var j = 0;
-
-if (n % 2 === 0 && !Math.floor(n / 10)) {
-    i += 1
-}
-if (n % 3 === 0 && n % 10 === 1) {
-    j += 1
+//5.Given a positive number. Print it in reverse order.
+function reversenumber(number){
+    let reversednumber = 0
+    while (number > 0) {
+        reversednumber =  reversednumber * 10 + number % 10 
+        number = Math.floor(number / 10)
+    }
+    console.log(reversednumber)
 }
