c715e142227ae8b703854dff972c9b4d4e04f7dd
AniTigranyan <144479495+AniTigranyan@users.noreply.github.com>
2023-12-08 16:23:29 +0400

    Add files via upload

--- homework.js
+++ homework.js
@@ -1,54 +1,53 @@
-//1.Given a number n (0 ≤ n ≤ 100) you should print the smallest positive number such that
-//its digits product is equal to n.
-
-
-
-//2.Find the biggest common divisor of two inserted numbers.
-
-//3.Given a number. Print all prime factors of the number.
-
-
-//4.Enter a positive number. Calculate and print its factorial. ( n! = n * (n-1) * (n-2) * … * 3 * 2
-//* 1 , 0! = 1 )
-function calculateFactorial(n) {
-    if (n < 0){
-        return false
-    }
-    else if (n === 0 || n === 1){
-        return 1
+//1.Given a string. Check whether the string is palindrome or not.
+function checkStr(str){
+    const length = str.length
+    for (let i = 0; i < length / 2; i++){
+        if (str[i] !== str[length - 1 - i]) {
+            return false
     }
     else {
-        let factorial = 1
-        for (let i = n; i >= 2; i--) {
-            factorial *= i
+        return true
+    }
+  }
+}
+
+//2.Given a string and symbols. Change first symbol by the second one in the string.
+function changeSimbols(str, firstSimbol, secondSimbol){
+
+}
+
+//3.Insert a string. Create new string which is the mirror reverse of the inserted one around
+//its center.
+function reverseStr(str) {
+    const center= Math.floor(str / 2)
+    let Array = []
+    for (let i = center - 1; i >= 0; i--) {
+        Array.push(str[i]);
+    }
+    if (str.length % 2 !== 0) {
+        center++;
+    }
+    for (let i = center; i < str.length; i++) {
+        Array.push(str[i]);
+    }
+}
+
+//4.Print the following number pattern: function will take argument (in this case it is 5)
+function printNumberPattern(n){
+    let row = ""
+    for (let i = 1; i <= n; i++){
+        for (let j = 1; j <= i; j++){
+            row += j
         }
-        console.log(factroial)
+        console.log(row)
+    }
+    for (let i = n - 1; i >= 1; i--){
+        let row = ""
+        for(let j = 1; j <= i; j++){
+            row += j
+        }
+        return row
     }
 }
 
-
-
-//5.Given a number n ( n >= 0 ). Print n Fibonacci number.
-function printFibonacciNum(n){
-    let a = 0
-    let b = 1
-    for (let i = 2; i <= n; i++) {
-        let sum = a + b
-        a = b
-        b = sum
-    }
-    return b 
-    
-}
-
-//6.Given a number n ( n > 0 ). Print Fibonacci series up to n.
-function printFibonacci(n){
-    a = 0 
-    b = 1
-    while (a + b <= n) {
-        let sum = a + b;
-        console.log(sum);
-        a = b;
-        b = sum;
-    }
-}
+//5.Given an object. Invert it (keys become values and values become keys).
